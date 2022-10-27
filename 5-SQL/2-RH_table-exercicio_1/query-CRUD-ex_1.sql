dae981df254e453cb2888fdadef16eb89410af65
danielscheicher@gmail.com <danielscheicher@gmail.com>
2022-10-27 10:59:05 -0300

    Updating query.sql

--- 5-SQL/2-RH_table-exercicio_1/query-CRUD-ex_1.sql
+++ 5-SQL/2-RH_table-exercicio_1/query-CRUD-ex_1.sql
@@ -15,19 +15,19 @@ DROP TABLE LOCATIONS;
 
 CREATE TABLE EMPLOYEES (
                           EMP_ID CHAR(9) NOT NULL,
-                          F_NAME VARCHAR(15) NOT NULL
+                          F_NAME VARCHAR(15) NOT NULL,
                           L_NAME VARCHAR(15) NOT NULL,
                           SSN CHAR(9),
                           B_DATE DATE,
                           SEX CHAR,
                           ADDRESS VARCHAR(30),
                           JOB_ID CHAR(9),
-                          SALARY DECIMAL(10,2) 
+                          SALARY DECIMAL(10,2),
                           MANAGER_ID CHAR(9),
                           DEP_ID CHAR(9) NOT NULL,
                           PRIMARY KEY (EMP_ID),
                           FOREIGN KEY (JOB_ID) REFERENCES JOB_HISTORY(JOB_ID),
-                          FOREIGN KEY (JOB_ID) REFERENCES Jobs(JOB_IDENT
+                          FOREIGN KEY (JOB_ID) REFERENCES Jobs(JOB_IDENT),
                           FOREIGN KEY (DEP_ID) REFERENCES Departments(DEPT_ID_DEP)
                         );
 
@@ -36,7 +36,7 @@ CREATE TABLE JOB_HISTORY (
                             START_DATE DATE,
                             JOBS_ID CHAR(9) NOT NULL,
                             DEPT_ID CHAR(9),
-                            PRIMARY KEY (EMPL_ID,JOBS_ID)
+                            PRIMARY KEY (EMPL_ID,JOBS_ID),
                             FOREIGN KEY (JOBS_ID) REFERENCES JOBS(JOB_IDENT)
                           );
 
@@ -74,7 +74,7 @@ INSERT INTO 'Employees' (EMP_ID,F_NAME,L_NAME,SSN,B_DATE,SEX,ADDRESS,JOB_ID,SALA
  ('E1004','Santosh','Kumar','123459','07/20/1985','M','511 Aurora Av, Aurora,IL','400','60000','30004','5'), 
  ('E1005','Ahmed','Hussain','123410','01/04/1981','M','216 Oak Tree, Geneva,IL','500','70000','30001','2'), 
  ('E1006','Nancy','Allen','123411','02/06/1978','F','111 Green Pl, Elgin,IL','600','90000','30001','2'), 
- ('E1007','Mary','Thomas','123412','05/05/1975','F','100 Rose Pl, Gary,IL','650','65000','30003','7'
+ ('E1007','Mary','Thomas','123412','05/05/1975','F','100 Rose Pl, Gary,IL','650','65000','30003','7'),
  ('E1008','Bharath','Gupta','123413','05/06/1985','M','145 Berry Ln, Naperville,IL','660','65000','30003','7'), 
  ('E1009','Andrea','Jones','123414','07/09/1990','F','120 Fall Creek, Gary,IL','234','70000','30003','7'), 
  ('E1010','Ann','Jacob','123415','03/30/1982','F','111 Britany Springs,Elgin,IL','220','70000','30004','5'),
@@ -87,13 +87,13 @@ INSERT INTO 'Employees' (EMP_ID,F_NAME,L_NAME,SSN,B_DATE,SEX,ADDRESS,JOB_ID,SALA
  ('E1017','Amanda','Thomas','123412','05/05/1975','F','100 Rose Pl, Gary,IL','650','65000','30003','7'),
  ('E1018','John ','Harry','123413','05/06/1985','M','145 Berry Ln, Naperville,IL','660','65000','30003','7'), 
  ('E1019','Andrea','Maria','123414','19/09/1990','F','120 Rua do Porto Lisboa,PT','780','70000','30003','10'),
- ('E1020','Roberta','Lima','123415','21/30/1982','F','120 Rua do Porto Lisboa,PT','780','70000','30004','10')
+ ('E1020','Roberta','Lima','123415','21/30/1982','F','120 Rua do Porto Lisboa,PT','780','70000','30004','10'),
  ('E1021','Mauricio','Helfstein','123414','19/09/1994','M','120 Rua do Brás, SP','000','70000','30003','00'),
  ('E1022','Lucas','Roberto','123415','21/30/1995','M','120 Rua da Mooca,SP','000','70000','30004','00');
 
 
 
-INSERT INTO 'JobsHistory' (EMPL_ID,START_DATE,JOBS_ID,DEPT_ID) VALUES 
+INSERT INTO 'Job_History' (EMPL_ID,START_DATE,JOBS_ID,DEPT_ID) VALUES 
  ('E1001','08/01/2000','100','2'), 
  ('E1002','08/01/2001','200','5'), 
  ('E1003','08/16/2001','300','5'), 
@@ -113,7 +113,7 @@ INSERT INTO 'JobsHistory' (EMPL_ID,START_DATE,JOBS_ID,DEPT_ID) VALUES
  ('E1016','08/16/2001','600','2'), 
  ('E1017','05/30/2002','650','7'), 
  ('E1018','05/06/2010','700','7'), 
- ('E1019','08/16/2016','780','10') 
+ ('E1019','08/16/2016','780','10'),
  ('E1020','08/16/2016','780','8');
  
 
@@ -138,7 +138,7 @@ INSERT INTO 'Departments' (DEPT_ID_DEP,DEP_NAME,MANAGER_ID,LOC_ID) VALUES
  ('5','Software Group','30002','L0002'), 
  ('7','Design Team','30003','L0003'),
  ('8','Software Group','30004','L0004'),
- ('9','Architect Group','30005','L0005') 
+ ('9','Architect Group','30005','L0005'),
  ('10','Design Team','30006','L0006');
 
 
